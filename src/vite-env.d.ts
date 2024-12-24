28877668a8622eab4ee98b68b06262ddb1b691a5
ms <maemotosato@gmail.coms>
2024-12-24 15:22:28 +0900

    fix

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_GRAPHQL_ENDPOINT: string;
+    readonly VITE_REGION: string;
+    readonly VITE_GRAPHQL_API_KEY: string;
+  }
+
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
