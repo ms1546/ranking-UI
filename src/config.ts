0e4968442a6dc4232c755147bc0774aa84cd0f70
ms <maemotosato@gmail.coms>
2024-12-24 15:39:55 +0900

    fix

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,9 @@
 const awsconfig: any = {
+    aws_project_region: import.meta.env.VITE_REGION,
     aws_appsync_graphqlEndpoint: import.meta.env.VITE_GRAPHQL_ENDPOINT,
     aws_appsync_region: import.meta.env.VITE_REGION,
     aws_appsync_authenticationType: "API_KEY",
     aws_appsync_apiKey: import.meta.env.VITE_GRAPHQL_API_KEY,
-};
+  };
 
-export default awsconfig;
+  export default awsconfig;
